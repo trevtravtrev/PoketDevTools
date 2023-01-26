201232173c2e4c042ff4258e942b928097e028a6
Trevor White <trevor.white@wayne.edu>
2023-01-25 19:34:54 -0500

    updated batch files (only work on my local machine)

--- projectgenerator/projectgenerator.bat
+++ projectgenerator/projectgenerator.bat
@@ -1,2 +1,2 @@
-C:\Users\trevo\OneDrive\Documents\GitHub\PoketDevTools\.venv\Scripts\python.exe "C:\Users\trevo\OneDrive\Documents\GitHub\PoketDevTools\projectgenerator\main.py"
+C:\Users\trevo\OneDrive\Documents\GitHub\PoketDevTools\venv\Scripts\python.exe "C:\Users\trevo\OneDrive\Documents\GitHub\PoketDevTools\projectgenerator\main.py"
 PAUSE
