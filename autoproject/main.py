27db8dee7e5697d354e2d090fff5f4a88ee99f70
Trevor White <trevor.white@wayne.edu>
2023-05-20 02:19:04 -0400

    Added updates for Poket Dev

--- autoproject/main.py
+++ autoproject/main.py
@@ -28,6 +28,12 @@ def create_gitignore(base_directory):
         f.write("*.idea\n")
 
 
+def create_requirements_txt(base_directory):
+    requirements_file = os.path.join(base_directory, "requirements.txt")
+    with open(requirements_file, "w") as file:
+        pass
+
+
 def create_project(
     project_name, project_description, poketdev_readme, project_path, github_username
 ):
@@ -54,20 +60,30 @@ def create_project(
             f"# {project_name} main file. Run this file to start the application. The Dockerfile is also preconfigured to run this file.\n"
         )
     print("Generated main.py file...")
+
+    # generate requirements.txt
+    create_requirements_txt(base_directory)
+    print("Generated requirements.txt file...")
+
+    # generate venv
+    subprocess.run(["poetry", "install"])
+    print("Generated poetry venv...")
+
     # Add packages using poetry
-    subprocess.run(
-        [
-            "poetry",
-            "add",
-            "poetry",
-            "flake8",
-            "black",
-            "radon",
-            "bandit",
-            "isort",
-            "mypy",
-        ]
-    )
+    # subprocess.run(
+    #     [
+    #         "poetry",
+    #         "add",
+    #         "poetry",
+    #         "flake8",
+    #         "black",
+    #         "radon",
+    #         "bandit",
+    #         "isort",
+    #         "mypy",
+    #     ]
+    # )
+
     # Go back to base directory
     os.chdir(base_directory)
     # Create .gitignore file
@@ -101,9 +117,119 @@ CMD ["poetry", "run", "python", "main.py"]"""
     os.chdir(base_directory)
     # generate readme
     with open("README.md", "w") as f:
-        # TODO: add poketdev readme
         if poketdev_readme:
-            f.write("")
+            f.write(
+                f"""# <br />
+<div align="center">
+  <a href="https://github.com/{github_username}/{project_name}">
+    <img src="logo.png" alt="{project_name}" width="500" height="100">
+  </a>
+
+  <p align="center">
+    {project_description}
+    <br/>
+  </p>
+</div>
+
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#prerequisites">Prerequisites</a></li>
+    <li><a href="#clone-the-repository">Clone the Repository</a></li>
+    <li><a href="#run-the-code">Run the Code</a></li>
+    <li><a href="#additional-tools">Additional Tools</a></li>
+    <li><a href="#thank-you-for-choosing-poket-dev!">Thank you for choosing Poket Dev!</a></li>
+  </ol>
+</details>
+
+# Contact
+For any project-related questions, bug fixes, feature requests, or communication needs regarding {project_name}, please utilize the comment section within your {project_name} request card on your Trello board. This centralizes all discussions, ensuring efficient and effective communication throughout the project's development.  
+
+# Prerequisites
+- [Python](https://www.python.org/downloads/) (latest version)  
+  - If using windows, in the python installer make sure to select the "Add Python to PATH" option  
+- [Pycharm](https://www.jetbrains.com/pycharm/download/#section=windows) (optional)
+
+
+# Clone the Repository
+## Option 1: via Command Line Interface
+- Install [GitHub CLI](https://cli.github.com/) (if not already installed)
+  ```
+  git clone https://github.com/{github_username}/{project_name}
+  ```
+## Option 2: via GitHub Desktop
+1. Install [GitHub Desktop](https://desktop.github.com/) (if not already installed)  
+2. Follow instructions [here](https://docs.github.com/en/desktop/contributing-and-collaborating-using-github-desktop/adding-and-cloning-repositories/cloning-a-repository-from-github-to-github-desktop) to clone
+# Run the Code
+## Option 1: Docker (recommended)
+
+Docker is a free tool that simplifies the process of running applications by packaging them with everything they need, so you can easily move them between different machines and ensure they work quickly and consistently, saving you time and reducing potential conflicts. If you're not familiar with docker, we STRONGLY recommend taking a few minutes to become familiar [here](https://www.docker.com/blog/getting-started-with-docker-desktop/#:~:text=Docker%20Desktop%20is%20an%20easy,%2C%20Kubernetes%2C%20and%20Credential%20Helper.).  
+1. Install [Docker Desktop](https://www.docker.com/products/docker-desktop/) (if not already installed)
+
+2. Build the Docker image:
+```
+cd {project_name}
+docker build -t {project_name} .
+```
+
+3. Run the Docker container:
+```
+docker run {project_name}
+```
+## Option 2: Poetry
+1. Install Poetry (if not already installed)
+```
+pip install poetry
+```
+2. Install {project_name} dependencies
+```
+cd {project_name}
+poetry install
+```
+3. Run {project_name}
+```
+poetry run python main.py
+```
+## Option 3: requirements.txt
+1. Create the virtual environment:
+```
+cd {project_name}
+python -m venv venv
+```
+2. Activate the virtual environment:
+- For Windows:
+```
+venv\Scripts\\activate
+```
+- For macOS/Linux:
+```
+source venv/bin/activate
+```
+Once activated, you will notice that the prompt in your terminal or command prompt changes to indicate that you are now working within the virtual environment.  
+3. Install {project_name} dependencies
+```
+pip install -r requirements.txt
+```
+4. Run {project_name}
+```
+python main.py
+```
+
+# Additional Tools
+All {project_name} code was formatted, linted, and secured with the following tools:
+- [black](https://black.readthedocs.io/en/stable/)
+- [flake8](https://flake8.pycqa.org/en/latest/)
+- [radon](https://radon.readthedocs.io/en/latest/)
+- [bandit](https://bandit.readthedocs.io/en/latest/)
+- [isort](https://pycqa.github.io/isort/)
+- [mypy](https://mypy.readthedocs.io/en/stable/)
+
+# Thank you for choosing Poket Dev!
+Thank you for choosing Poket Dev, LLC as your unlimited software development partner. We greatly appreciate you and are committed to delivering exceptional solutions and exceeding your expectations.  
+
+If you'd like to refer someone to Poket Dev, and they subscribe to any of our packages, you'll receive a $1000 referral bonus.  """
+            )
         else:
             f.write(
                 f"""# <br />
@@ -125,16 +251,19 @@ CMD ["poetry", "run", "python", "main.py"]"""
 
 
 def main():
-    project_name = input("Project name: ")
+    project_name = input("Project name (no whitespace, hyphens, or underscores): ")
     project_description = input("Project description: ")
     while True:
-        poketdev_readme = int(input("Readme (1 for PocketDev, 0 for regular): "))
+        poketdev_readme = int(input("README (0 for personal, 1 for Poket Dev): "))
         if poketdev_readme not in [1, 0]:
             print("Invalid option.")
             continue
         else:
             break
-    github_username = input("Github Username: ")
+    if poketdev_readme == 0:
+        github_username = input("Github Username: ")
+    else:
+        github_username = input("Organization Name (ex. PoketDev-YourCompanyName): ")
     project_path = input("Project Target Path: ")
     create_project(
         project_name,
